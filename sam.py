e2dee642840e008f400555787eaf96e0ee3dd84b
priyanka vaidya <81521391+priyankavaidya261@users.noreply.github.com>
2023-01-31 17:47:13 +0530

    Add files via upload

---
+++ sam.py
@@ -0,0 +1,13 @@
+import random 
+from tkinter import *
+root = Tk()
+root.geometry("700x450")
+l1= Label(root,font=("times",200))
+
+def roll():
+	num = ['\u2680','\u2861','\u2682','\u2683','\u2684','\u2685']
+	l1.config(text=f'{random.choice(num)}{random.choice(num)}')
+	l1.pack()
+b1 = Button(root,text="lets roll..",command=roll)
+b1.place(x=350,y=0)
+root.mainloop()
